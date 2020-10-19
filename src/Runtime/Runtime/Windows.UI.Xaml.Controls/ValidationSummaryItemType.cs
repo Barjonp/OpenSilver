f7819152dec9774b8dc6a1a867f70f42b9890f88
Pierre Barjon <barjon.pierre@gmail.com>
2020-10-19 23:55:56 +0200

    Implement ValidationSummary (Silverlight toolkit).

---
+++ src/Runtime/Runtime/Windows.UI.Xaml.Controls/ValidationSummaryItemType.cs
@@ -0,0 +1,34 @@
+//-----------------------------------------------------------------------
+// <copyright company="Microsoft">
+//      (c) Copyright Microsoft Corporation.
+//      This source is subject to the Microsoft Public License (Ms-PL).
+//      Please see http://go.microsoft.com/fwlink/?LinkID=131993 for details.
+//      All other rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+#if MIGRATION
+namespace System.Windows.Controls
+#else
+namespace Windows.UI.Xaml.Controls
+#endif
+{
+    /// <summary>
+    /// The source of the error, for error management
+    /// </summary>
+    /// <QualityBand>Preview</QualityBand>
+    [global::System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA1801", Justification = "ValidationSummaryItemType of None is not permitted.")]
+    [global::System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1008", Justification = "ValidationSummaryItemType of None is not permitted.")]
+    public enum ValidationSummaryItemType
+    {
+        /// <summary>
+        /// The error came from object level validation
+        /// </summary>
+        ObjectError = 1,
+
+        /// <summary>
+        /// The error came from the binding engine, which exposes only a single error at a time
+        /// </summary>
+        PropertyError = 2
+    }
+}
